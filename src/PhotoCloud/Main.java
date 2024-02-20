01c23b079ba7ffbd9c06adc12a39789d5882a75e
Michael Shamshidov <115317256+XT3RM1NATOR@users.noreply.github.com>
2024-02-20 10:29:07 +0500

    Update Main.java

--- src/PhotoCloud/Main.java
+++ src/PhotoCloud/Main.java
@@ -3,15 +3,6 @@ import PhotoCloud.Login.*;
 
 public class Main {
     public static void main(String[] args) {
-    	/************** Pledge of Honor ******************************************
-    	I hereby certify that I have completed this programming project on my own
-    	without any help from anyone else. The effort in the project thus belongs
-    	completely to me. I did not search for a solution, or I did not consult any
-    	program written by others or did not copy any program from other sources. I
-    	read and followed the guidelines provided in the project description.
-    	READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID
-    	SIGNATURE: <Mikhail Shamshidov, 0082094>
-    	*************************************************************************/
         IDandPassword idandPasswords = new IDandPassword("Accounts.txt");
         LoginPage loginPage = new LoginPage(IDandPassword.getInfo()); // initializing everything
         
